afa4afb916981e1e8ea20cc52955610bd74b7b08
sebastienjaz3 <sebastien.jaz3@gmail.com>
2026-06-08 21:32:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,18 +30,18 @@ app_ui = ui.page_fluid(
         ),
         ui.output_plot("stock_plot"),
         ui.markdown("""
-                    - FIAT --------- 'CADTHB=X', 'USDTHB=X', 'EURTHB=X', 'USDCAD=X', 'EURCAD=X', 'CADUSD=X', 'CADEUR=X'
-                    - CRYPTO ------- 'BTC-USD', 'ETH-USD', 'XRP-USD'
-                    - AI ----------- 'PLTR', 'OPAI.PVT', 'ANTH.PVT'
-                    - S&P500 ------- '^GSPC', '^SPX', 'SPY', 'IVV', 'VOO', 'VFIAX'
-                    - NASDAQ ------- '^IXIC', '^NDX', 'QQQ', 'QQQM'
-                    - DOWJONES ----- '^DJI'
-                    - VANGUARD ----- 'VT', 'VTI', 'VGT', 'VYM', 'BLK'
-                    - BLACKROCK ---- 'BLK'
-                    - DOLLARAMA ---- 'DOL.TO', 'DLMAF'
-                    - COUCHETARD --- 'ATD.TO'
-                    - RETAIL ------- 'WMT', 'TGT', 'COST', 'BJ', 'KR', 'DG'
-                    - TECH --------- 'TSLA', 'NVDA', 'GOOGL', 'AAPL', 'META', 'AMZN', 'MSFT'
+                    - FIAT : 'CADTHB=X', 'USDTHB=X', 'EURTHB=X', 'USDCAD=X', 'EURCAD=X', 'CADUSD=X', 'CADEUR=X'
+                    - CRYPTO : 'BTC-USD', 'ETH-USD', 'XRP-USD'
+                    - AI : 'PLTR', 'OPAI.PVT', 'ANTH.PVT'
+                    - S&P500 : '^GSPC', '^SPX', 'SPY', 'IVV', 'VOO', 'VFIAX'
+                    - NASDAQ : '^IXIC', '^NDX', 'QQQ', 'QQQM'
+                    - DOWJONES : '^DJI'
+                    - VANGUARD : 'VT', 'VTI', 'VGT', 'VYM', 'BLK'
+                    - BLACKROCK : 'BLK'
+                    - DOLLARAMA : 'DOL.TO', 'DLMAF'
+                    - COUCHETARD : 'ATD.TO'
+                    - RETAIL : 'WMT', 'TGT', 'COST', 'BJ', 'KR', 'DG'
+                    - TECH : 'TSLA', 'NVDA', 'GOOGL', 'AAPL', 'META', 'AMZN', 'MSFT'
                      """)
     )
 )
