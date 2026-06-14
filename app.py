8dfaa5d45b141685fde0bf25b4e9bb83bf5f9ceb
sebjazdev <sebastien.jaz3@gmail.com>
2026-06-14 14:57:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ app_ui = ui.page_fluid(
             ui.input_date("start_cal_date", ui.tags.b("Select Start Date"), value="2026-01-01"),
             ui.input_date("end_cal_date", ui.tags.b("Select End Date"), value=date.today()),
             # ui.input_checkbox("select_all", ui.tags.b("Select All/None"), value=False),
-            ui.input_checkbox("select_preferred", ui.tags.b("Select preferred"), value=True),
+            ui.input_checkbox("select_preferred", ui.tags.b("Select Preferred"), value=True),
             ui.input_checkbox_group("tickers", ui.tags.b("Select Tickers"), choices={t: t for t in AVAILABLE_TICKERS}) #, selected=["GLD", "SPY"])
         ),
         ui.output_plot("stock_plot"),
