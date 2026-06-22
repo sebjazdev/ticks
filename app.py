3587f5ea7609ffddfd3b9b9f721c6483fc04366b
sebjazdev <sebastien.jaz3@gmail.com>
2026-06-22 10:14:21 +0700

    Add TSM, SPCX, and 005930 to tech stocks list

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ AVAILABLE_TICKERS = ['CADTHB=X', 'USDTHB=X', 'EURTHB=X', 'USDCAD=X', 'EURCAD=X',
                      'DOL.TO', 'DLMAF', # DOLLARAMA
                      'ATD.TO', # COUCHETARD
                      'WMT', 'COST', 'TGT', 'BJ', 'KR', 'DG', 'HD', # RETAIL
-                     'TSLA', 'NVDA', 'GOOGL', 'AAPL', 'META', 'AMZN', 'MSFT', # TECH
+                     'TSLA', 'NVDA', 'GOOGL', 'AAPL', 'META', 'AMZN', 'MSFT', 'TSM', 'SPCX', '005930', # TECH
                      'MRVL', 'DIS', 'NFLX', 'SONY' # ENTERTAINMENT
                     ]
 
